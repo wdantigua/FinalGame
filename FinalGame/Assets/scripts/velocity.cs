c97ab5a386ced75143367545647426edee644f92
yingtingyang <yangy389@newschool.edu>
2016-11-11 21:22:25 -0500

    *sings* never gonna git it never gonna git it!

---
+++ FinalGame/Assets/scripts/velocity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class velocity : MonoBehaviour {
+
+    public Vector2 velo = new Vector2(-4, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D.velo = velocity;
+    }
+}
+
